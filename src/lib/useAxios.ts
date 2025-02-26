5ee8d6342c6482c30c41a57b27c8deff305e9337
0xZisu <lazy1523@outlook.com>
2025-02-26 22:27:54 +0800

    update fix use axios

--- src/lib/useAxios.ts
+++ src/lib/useAxios.ts
@@ -11,7 +11,7 @@ const useAxios = () => {
 
     // 创建axios实例
     const instance = axios.create({
-        baseURL: 'http://localhost:3000/',
+        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
         timeout: 10000,
         headers: {
             'Content-Type': 'application/json',
