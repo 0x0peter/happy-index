b41a90c5c1a8cecdb1cf12daa1fd98049c6ae448
0xZisu <lazy1523@outlook.com>
2025-02-28 03:11:42 +0800

    update fix bug :

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,8 @@ import Layout from "@/components/sophon/layout/layout";
 // import Loading from "@/components/sophon/loading/loading";
 import { Toaster } from "@/components/ui/toaster";
 import "@/src/styles/globals.css";
-import { userState } from "@/store/globalState";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { RecoilRoot, useRecoilState } from "recoil";
+import { RecoilRoot } from "recoil";
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
@@ -27,9 +25,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <RecoilRoot>
-            <Layout>
+            {/* <Layout> */}
               <Component {...pageProps} />
-            </Layout>
+            {/* </Layout> */}
             <Toaster />
           </RecoilRoot>
         </RainbowKitProvider>
