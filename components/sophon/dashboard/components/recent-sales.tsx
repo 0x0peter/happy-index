3917b699a5edbe42f7e3c8d6fc806f0a1f89f30b
0xZisu <lazy1523@outlook.com>
2025-02-27 16:22:31 +0800

    add Blockie to address

--- components/sophon/dashboard/components/recent-sales.tsx
+++ components/sophon/dashboard/components/recent-sales.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import makeBlockie from "ethereum-blockies-base64";
 
 
 /**
@@ -18,10 +19,25 @@ export function RecentSales({rankList}:{rankList:any}) {
     <div className="space-y-8">
       {rankList.map((item:any,index:any)=>(
           <div key={index} className="flex items-center">
-          <Avatar className="h-9 w-9">
-            <AvatarImage src="/avatars/01.png" alt="Avatar" />
-            <AvatarFallback>{item.rank}</AvatarFallback>
-          </Avatar>
+           <Avatar className="h-12 w-12 flex-shrink-0">
+                      <AvatarImage src={makeBlockie(item.walletAddress)} alt="Avatar" />
+                      <AvatarFallback>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="h-4 w-4 text-muted-foreground"
+                        >
+                          <rect width="7" height="18" x="3" y="3" rx="1" />
+                          <rect width="7" height="7" x="14" y="3" rx="1" />
+                          <rect width="7" height="7" x="14" y="14" rx="1" />
+                        </svg>
+                      </AvatarFallback>
+                    </Avatar>
           <div className="ml-4 space-y-1">
             <p className="text-sm font-medium leading-none">    {formatAddress(item.walletAddress) }</p>
             <p className="text-sm text-muted-foreground">
--- components/sophon/dashboard/components/team-members-list.tsx
+++ components/sophon/dashboard/components/team-members-list.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import makeBlockie from "ethereum-blockies-base64";
 import React from "react";
 
 
@@ -20,10 +21,26 @@ export const TeamMembersList = React.memo(({memberList}:{memberList:any}) => {
     <div className="space-y-8">
       {memberList.map((item:any,index:any)=>(
           <div key={index} className="flex items-center">
-          <Avatar className="h-9 w-9">
-            <AvatarImage src="/avatars/01.png" alt="Avatar" />
-            <AvatarFallback>{item.rank}</AvatarFallback>
-          </Avatar>
+            <Avatar className="h-12 w-12 flex-shrink-0">
+                      <AvatarImage src={makeBlockie(item.walletAddress)} alt="Avatar" />
+                      <AvatarFallback>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="h-4 w-4 text-muted-foreground"
+                        >
+                          <rect width="7" height="18" x="3" y="3" rx="1" />
+                          <rect width="7" height="7" x="14" y="3" rx="1" />
+                          <rect width="7" height="7" x="14" y="14" rx="1" />
+                        </svg>
+                      </AvatarFallback>
+                    </Avatar>
+          
           <div className="ml-4 space-y-1">
             <p className="text-sm font-medium leading-none">    {formatAddress(item.walletAddress) }</p>
             <p className="text-sm text-muted-foreground">
