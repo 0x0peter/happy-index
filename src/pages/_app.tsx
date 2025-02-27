edb811ce2e732e523c7286778e27e233fad0c8bf
0xZisu <lazy1523@outlook.com>
2025-02-28 02:45:08 +0800

    update3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,71 +6,34 @@ import { userState } from "@/store/globalState";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { RecoilRoot, useRecoilState } from "recoil";
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ComponentWithUser = ({ Component, pageProps }: AppProps) => {
-    const router = useRouter();
-    const [, setUser] = useRecoilState(userState);
-
-    const accessibleRoutesWhenNotLoggedIn = [
-      "/auth/login",
-      "/auth/forgot",
-      "/auth/register",
-    ];
-
-
-    if (accessibleRoutesWhenNotLoggedIn.includes(router.pathname)) {
-      return <Component {...pageProps} />;
-    }
-
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  };
-
   const config = getDefaultConfig({
-    appName: 'HappyIndex',
-    projectId:'a46f7878ece2f9eaec319f8f850320ff',
+    appName: "HappyIndex",
+    projectId: "a46f7878ece2f9eaec319f8f850320ff",
     chains: [mainnet, polygon, optimism, arbitrum, base],
     ssr: true, // If your dApp uses server side rendering (SSR)
   });
   const queryClient = new QueryClient();
 
   return (
-    
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
-        <RecoilRoot>
-        <ComponentWithUser Component={Component} {...pageProps} />
-        <Toaster />
-        </RecoilRoot>
+          <RecoilRoot>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+            <Toaster />
+          </RecoilRoot>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
-    
-   
-
-   
   );
 }
